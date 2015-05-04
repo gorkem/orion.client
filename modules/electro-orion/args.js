27af32ce97aba94d74e8af82754119eb33ff3f98
Gorkem Ercan <gorkem.ercan@gmail.com>
2015-05-04 16:12:05 -0400

    make current directory to be default when unspecified
    Signed-off-by: Gorkem Ercan <gorkem.ercan@gmail.com>

--- modules/electro-orion/args.js
+++ modules/electro-orion/args.js
@@ -2,7 +2,7 @@
 /*eslint-env node */
 
 exports.parseArgs= function (argv){
-    var args = {};
+    var args = {dir:'.'};
     argv = argv.slice(2); //remove node and script
     for(var i =0; i<argv.length; i++){
     	args.dir = argv[i];
